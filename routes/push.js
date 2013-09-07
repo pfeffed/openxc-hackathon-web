941e8b5267e005b6e60b5e56f1d2dfb6cb68a5f1
David Jensen <david@jensen47.com>
2013-09-07 13:09:21 -0700

    Added a simple URL to send a push message.

---
+++ routes/push.js
@@ -0,0 +1,25 @@
+var routes = function(app) {
+  requireRoutes(__dirname, app),
+    gcm = require('node-gcm');
+
+  // Defines the root page. can be safely removed!
+  app.get('/push', function(req, res) {
+    var message = new gcm.Message({
+      collapseKey: 'demo',
+      delayWhileIdle: true,
+      timeToLive: 3,
+      data: {
+        foo: 'bar',
+        hello: 'world'
+      }
+    })
+    var sender = new gcm.Sender('insert Google Server API Key here');
+    var registrationIds = [];
+    registrationIds.push('regId1');
+    sender.send(message, registrationIds, 4, function (err, result) {
+      console.log(result);
+    });
+  });
+}
+
+module.exports = routes;
